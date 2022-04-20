670fcb815003e3c9aad0ec4127cabf36994d9722
NIKKI256 <nekitborov@gmail.com>
2022-04-20 19:13:41 +0300

    add consts, union bots, add filters

--- compliment_bot.py
+++ compliment_bot.py
@@ -1,34 +1,40 @@
-from pyrogram import Client
-from random import choice
+from pyrogram import Client, filters
 from decouple import config
+from consts import compliments, agree_reactions
+from random import choice, randrange, getrandbits
+import asyncio
 
 APP_ID = config('APP_ID')
 APP_HASH = config('APP_HASH')
+
 LOVE_ID = config('LOVE_ID', cast=int)
+TOLIK_ID = config('TOLIK', cast=int)
 
 app = Client('my_account', api_id=APP_ID, api_hash=APP_HASH)
 
-compliments = [
-    'Боже, какая же ты красивая',
-    'Ты с каждым видео все лучше и лучше',
-    'У меня сейчас будет передоз красоты в мозгах',
-    'Именно ради такой и надо становиться богатым',
-    'Вообще законно быть такой красивой?',
-    'Я мог бы всю жизнь думать о тебе и мне бы не надоело'
-]
 
-@app.on_message()
-def react_to_video_message(_, message):
+@app.on_message(filters.user(users=LOVE_ID))
+async def react_to_video_message_of_love(_, message):
     # After it you can comment lines below 
     # print(f'Hello here you can find out id of your love {message.from_user.id}')
     # print(f'Here is name of person {message.from_user.first_name}')
     # print(f'Here is username of person {message.from_user.username}')
     #
-    is_lovely_person = message.from_user.id == LOVE_ID
     is_video_message = getattr(message, 'video_note', False)
     #
-    if is_lovely_person and is_video_message:
+    if is_video_message:
         compliment_message = choice(compliments)
-        message.reply(compliment_message)
+        await message.reply(compliment_message)
+
+
+@app.on_message(filters.user(users=TOLIK_ID))
+async def react_to_message(_, message):
+    #
+    is_react_to_message = bool(getrandbits(1))
+    #
+    if is_react_to_message:
+        await asyncio.sleep(randrange(3,5))
+        reaction = choice(agree_reactions)
+        await message.reply(reaction)
 
 app.run()
